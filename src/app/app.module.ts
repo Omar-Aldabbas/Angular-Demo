0a4835cb71ee4bc8b84712790117ab840b9a6ddd
Omar Aldabbas <omaraldabbassjo@gmail.com>
2025-11-23 00:39:01 -0800

    2: fn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,17 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-import { CardComponent } from './shared/card/card.component';
-import { FormsModule, NgModel } from '@angular/forms';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { CardModule } from './shared/card.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @NgModule({
   //declarations only for fully stand alone component
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    CardComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-  
-  ],
+  declarations: [AppComponent, HeaderComponent, UserComponent],
   bootstrap: [AppComponent],
   // imports in the other hand is for imported component thats not stand alone
 
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, CardModule, TasksModule],
 })
 export class AppModule {}
